be78ed7f457e344d99582a40309d888e502e7829
Matej Vit <329matej329@gmail.com>
2024-03-27 18:01:09 +0100

    Loading

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ function App() {
 
   useEffect(() => {
     setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
   }, [])
 
   useEffect(() => {
@@ -27,37 +30,38 @@ function App() {
   return (
     <>
         {loading ? (
-          <main>
-            <div className="title__wrap">
-              <h1 className={`${animation ? 'title-active' : ''} title`}>TO THE SUN V2</h1>
-            </div>
-            <p className='welcome'>WELCOME TO TTS2.0!</p>
-            <div className='icon__wrapper'>
-              <a href="https://www.instagram.com/to_the_sun_v2/" target='_blank'>
-                <IoLogoInstagram className='icon'/>
-              </a>
-              <a href="https://twitter.com/ToTheSunV2" target='_blank'>
-                <IoLogoTwitter className='icon'/>
-              </a>
-              <a href="https://t.me/+ozyyK2fXv0kzOWQx" target='_blank'>
-                <FaTelegram className='icon'/>
-              </a>
-              <a href="https://dexscreener.com/solana/4QWvNnFrDPgdXbcoCkdfb1JbXWefLkCYmnJXiHS9bKhg" target='_blank'>
-                <img src={dexImg} alt="dexscreener icon" className='dexscreener'/>
-              </a>
-            </div>
-            <a href='https://raydium.io/swap/?inputCurrency=sol&outputCurrency=7nXJVZonVx8kQeNdMJk9VCaqC1skSTajhrAxJt4SkLTT&fixed=in' target='blank' className='buy__button'>BUY</a>
-            <Spline scene="https://prod.spline.design/oddaRVMOdOKywdEB/scene.splinecode" />
-            <p className='description'>JOIN OUR RESILIENT CRYPTO PROJECT BORN<br />FROM THE ASHES OF A RUG PULL SCAM</p>
-            <p className='description'>WE'RE DEDICATED TO RESTORING FAITH IN CRYPTO<br />INVESTMENTS AFTER THE BETRAYAL OF TTS 1.0</p>
-            <p className='description'>GET READY FOR A NEW ERA OF TRANSPARENCY AND<br />SECURITY AS WE LAUNCH OUR OWN COIN</p>
-          </main>
-        ) : <div className='loading'>
-        <l-bouncy
-          size="45"
-          speed="1.75" 
-          color="white" 
-        ></l-bouncy></div>}
+          <div className='loading'>
+          <l-bouncy
+            size="45"
+            speed="1.75" 
+            color="white" 
+          ></l-bouncy></div>
+          
+        ) : <main>
+        <div className="title__wrap">
+          <h1 className={`${animation ? 'title-active' : ''} title`}>TO THE SUN V2</h1>
+        </div>
+        <p className='welcome'>WELCOME TO TTS2.0!</p>
+        <div className='icon__wrapper'>
+          <a href="https://www.instagram.com/to_the_sun_v2/" target='_blank'>
+            <IoLogoInstagram className='icon'/>
+          </a>
+          <a href="https://twitter.com/ToTheSunV2" target='_blank'>
+            <IoLogoTwitter className='icon'/>
+          </a>
+          <a href="https://t.me/+ozyyK2fXv0kzOWQx" target='_blank'>
+            <FaTelegram className='icon'/>
+          </a>
+          <a href="https://dexscreener.com/solana/4QWvNnFrDPgdXbcoCkdfb1JbXWefLkCYmnJXiHS9bKhg" target='_blank'>
+            <img src={dexImg} alt="dexscreener icon" className='dexscreener'/>
+          </a>
+        </div>
+        <a href='https://raydium.io/swap/?inputCurrency=sol&outputCurrency=7nXJVZonVx8kQeNdMJk9VCaqC1skSTajhrAxJt4SkLTT&fixed=in' target='blank' className='buy__button'>BUY</a>
+        <Spline scene="https://prod.spline.design/oddaRVMOdOKywdEB/scene.splinecode" />
+        <p className='description'>JOIN OUR RESILIENT CRYPTO PROJECT BORN<br />FROM THE ASHES OF A RUG PULL SCAM</p>
+        <p className='description'>WE'RE DEDICATED TO RESTORING FAITH IN CRYPTO<br />INVESTMENTS AFTER THE BETRAYAL OF TTS 1.0</p>
+        <p className='description'>GET READY FOR A NEW ERA OF TRANSPARENCY AND<br />SECURITY AS WE LAUNCH OUR OWN COIN</p>
+      </main>}
     </>
   )
 }
